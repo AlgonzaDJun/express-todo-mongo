380a4a83dd9f9c30fb580ebf8bded3186ced7d63
AlgonzaDJun <algonsadewangga@gmail.com>
2023-11-10 12:59:47 +0700

    cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const db = require("./config/db");
 const { allRouter } = require("./routes");
 dotenv.config();
@@ -15,7 +16,8 @@ db.then(() => {
 });
 
 app.use(express.json());
-app.use(allRouter)
+app.use(cors());
+app.use(allRouter);
 app.listen(port, () => {
   console.log(`server running on port ${port}`);
 });
